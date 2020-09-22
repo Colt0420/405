1fce30900ce15f2b899968d2ee83d2244ece626a
Colt Liu <colt.liu7@gmail.com>
2020-09-21 18:50:24 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import React from "react"
-import logo from "./logo.svg"
+import logo from "./logo.png"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Vanity</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1 style={{ marginTop: "0" }}>Vanity Plate</h1>
+        <img
+          style={{ margin: 10 }}
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+        <p style={{ fontSize: "26px" }}>
           Currently, to receive a vanity license plate, applicants must list
           their top three choices and receive the highest available one blind.
-          This process is also done all by paper. We aim to provide a way to
-          know ahead of time and complete the process online.
+          This process is all done by paper. We aim to provide a way to know
+          ahead of time and complete the process online.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://forms.gle/ucffKTtEBnMvn9jf6"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Link to the form
-        </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link to the Survey
+          Please take the survey
         </a>
       </header>
     </div>
