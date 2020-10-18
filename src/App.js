ac0915220f9e77ecd2ed1d3dbb050b61eb03faad
Colt Liu <colt.liu7@gmail.com>
2020-10-18 16:13:34 -0600

    added name and email form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
 import logo from "./logo.png"
 import "./App.css"
+import TextField from "@material-ui/core/TextField"
+import { makeStyles } from "@material-ui/core/styles"
+import Button from "@material-ui/core/Button"
+import grey from "@material-ui/core/colors/grey"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+      background: "white",
+    },
+  },
+  textField: {
+    color: "black",
+  },
+}))
 
 function App() {
+  const classes = useStyles()
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+
+  // const updateName = (input) => {
+  //   setName(input.text)
+  // }
+
+  // const updateEmail = (input) => {
+  //   setEmail(input.text)
+  // }
+
+  const clear = () => {
+    setName("")
+    setEmail("")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -25,8 +59,39 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Please take the survey
+          Link to survey
         </a>
+
+        <TextField
+          error
+          key="Confirmation Code"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          id="outlined-error-helper-text"
+          label="Name"
+          variant="outlined"
+        />
+
+        <TextField
+          error
+          key="Confirmation Code"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          id="outlined-error-helper-text"
+          label="Email"
+          variant="outlined"
+        />
+
+        <Button
+          onClick={() => {
+            clear()
+            alert("Thank you for your response")
+          }}
+          variant="contained"
+          color="primary"
+        >
+          Submit
+        </Button>
       </header>
     </div>
   )
